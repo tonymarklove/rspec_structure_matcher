e4086613761a9e63b2116ecf61f2544d9617f72d
Tony Marklove <tony@fluxbit.co.uk>
2016-06-23 16:43:22 +0100

    Support nesting of structures with nice diff output

--- lib/have_structure_matcher.rb
+++ lib/have_structure_matcher.rb
@@ -1,30 +1,10 @@
 RSpec::Matchers.define :have_structure do |expected, opts|
   match do |actual|
-    invalid_items(actual, expected).empty?
+    HaveStructureMatcher.match?(actual, expected)
   end
 
   failure_message do |actual|
-    invalid = invalid_items(actual, expected)
-    "missing or invalid keys: #{invalid.keys.join(', ')}\n\n#{invalid}"
-  end
-
-  def invalid_items(actual, expected)
-    expected.each_with_object({}) do |(key, value), memo|
-      if !has_key?(actual, key) || !have_structure_value_match?(get_key(actual, key), value)
-        memo[key] = value
-      end
-    end
-  end
-
-  def has_key?(actual, key)
-    actual.key?(key.to_s) || actual.key?(key.to_sym)
-  end
-
-  def get_key(actual, key)
-    if actual.key?(key.to_s)
-      actual[key.to_s]
-    else
-      actual[key.to_sym]
-    end
+    differ = RSpec::Support::Differ.new(color: RSpec::Matchers.configuration.color?)
+    differ.diff(actual, HaveStructureMatcher.build_diff(actual, expected))
   end
 end
--- lib/support_methods.rb
+++ lib/support_methods.rb
@@ -1,9 +1,47 @@
-module HaveStructureMatcherMethods
-  def optionally(optional_expected_value)
-    ->(actual_value) { actual_value.nil? || have_structure_value_match?(actual_value, optional_expected_value) }
+module HaveStructureMatcher
+  module Methods
+    def optionally(optional_expected_value)
+      ->(actual_value) { actual_value.nil? || HaveStructureMatcher.match?(actual_value, optional_expected_value) }
+    end
   end
 
-  def have_structure_value_match?(actual_value, expected_value)
+  def self.match?(actual, expected)
+    actual == build_diff(actual, expected)
+  end
+
+  def self.build_diff(actual, expected)
+    return expected unless actual.is_a?(Hash)
+
+    keys = actual.keys | expected.keys
+
+    keys.each_with_object({}) do |key, memo|
+      if actual.key?(key) && expected.key?(key)
+        actual_value = actual[key]
+        expected_value = expected[key]
+
+        if expected_value.is_a?(Hash)
+          memo[key] = build_diff(actual_value, expected_value)
+        elsif value_match?(actual_value, expected_value)
+          memo[key] = actual_value
+        else
+          memo[key] = print_expected_value(expected_value)
+        end
+      elsif expected.key?(key)
+        expected_value = expected[key]
+        memo[key] = print_expected_value(expected_value)
+      end
+    end
+  end
+
+  def self.print_expected_value(expected_value)
+    if expected_value.respond_to?(:call)
+      "Proc [#{expected_value.source_location.join(':')}]"
+    else
+      expected_value
+    end
+  end
+
+  def self.value_match?(actual_value, expected_value)
     if expected_value.is_a?(Regexp)
       actual_value =~ expected_value
     elsif expected_value.is_a?(Class)
@@ -17,5 +55,5 @@ module HaveStructureMatcherMethods
 end
 
 RSpec.configure do |config|
-  config.include HaveStructureMatcherMethods
+  config.include HaveStructureMatcher::Methods
 end
